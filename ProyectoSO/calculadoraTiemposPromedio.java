771ec042ca771ddfcae10567ab1a804fa8692d69
Angel Raya <111707135+AneglouS@users.noreply.github.com>
2025-10-25 20:20:08 -0600

    Add files via upload
    Clases base.

---
+++ ProyectoSO/calculadoraTiemposPromedio.java
@@ -0,0 +1,18 @@
+package ProyectoSO;
+
+public class calculadoraTiemposPromedio {
+    public double calcularTiempoDeRespuesta() {
+        // Lógica para calcular el tiempo de respuesta promedio
+        return 0.0;
+    }
+
+    public double calcularTiempoDeEspera() {
+        // Lógica para calcular el tiempo de espera promedio
+        return 0.0;
+    }
+
+    public double calcularTiempoDeEjecucion() {
+        // Lógica para calcular el tiempo de ejecución promedio
+        return 0.0;
+    }
+} 
